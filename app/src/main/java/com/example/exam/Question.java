ecad0a5fdd6e852b172486736835c0829570abc9
cswyxiang <1353281061@qq.com>
2017-03-01 23:14:42 +0800

    1

---
+++ app/src/main/java/com/example/exam/Question.java
@@ -0,0 +1,92 @@
+package com.example.exam;
+
+import cn.bmob.v3.BmobObject;
+
+/**
+ * Created by 魏于翔 on 2017/2/4.
+ */
+
+public class Question extends BmobObject{
+
+    private int ID;
+    private String question;
+    private String answerA;
+    private String answerB;
+    private String answerC;
+    private String answerD;
+    private int answer;
+    private String explaination;
+    private int selectedAnswer;
+
+    public int getID() {
+        return ID;
+    }
+
+    public void setID(int ID) {
+        this.ID = ID;
+    }
+
+    public String getQuestion() {
+        return question;
+    }
+
+    public void setQuestion(String question) {
+        this.question = question;
+    }
+
+    public String getAnswerA() {
+        return answerA;
+    }
+
+    public void setAnswerA(String answerA) {
+        this.answerA = answerA;
+    }
+
+    public String getAnswerB() {
+        return answerB;
+    }
+
+    public void setAnswerB(String answerB) {
+        this.answerB = answerB;
+    }
+
+    public String getAnswerC() {
+        return answerC;
+    }
+
+    public void setAnswerC(String answerC) {
+        this.answerC = answerC;
+    }
+
+    public String getAnswerD() {
+        return answerD;
+    }
+
+    public void setAnswerD(String answerD) {
+        this.answerD = answerD;
+    }
+
+    public int getAnswer() {
+        return answer;
+    }
+
+    public void setAnswer(int answer) {
+        this.answer = answer;
+    }
+
+    public String getExplaination() {
+        return explaination;
+    }
+
+    public void setExplaination(String explaination) {
+        this.explaination = explaination;
+    }
+
+    public int getSelectedAnswer() {
+        return selectedAnswer;
+    }
+
+    public void setSelectedAnswer(int selectedAnswer) {
+        this.selectedAnswer = selectedAnswer;
+    }
+}
